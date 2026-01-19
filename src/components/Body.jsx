fe9d06213f5662ee854eb3cb09392fb9738d7e4d
Nihal SANDHU <nihalsandhu007220@gmail.com>
2026-01-19 15:06:24 +0530

    routing

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+const Body = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
